49b6a7bf987b8babf82c9560c82428c03e97ce00
Chimezie Enyinnaya <meziemichael@gmail.com>
2016-12-19 20:38:44 +0100

    add Facade

---
+++ src/Facades/Paystack.php
@@ -0,0 +1,18 @@
+<?php 
+
+namespace Ammezie\Paystack\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+class Paystack extends Facade
+{
+    /**
+     * Get the registered name of the component.
+     *
+     * @return string
+     */
+    protected static function getFacadeAccessor()
+    {
+        return 'paystack-laravel';
+    }
+}
